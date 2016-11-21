3572d3164b3725c57339ce7820c3898bee4eabfb
maksimgm <maksimmamrikov@gmail.com>
2016-11-20 18:09:17 -0800

    set up working server. move static files to public directory

--- server.js
+++ server.js
@@ -1,4 +1,28 @@
-var express = require('express'),
-  app = express(),
-  http = require('http').Server(app),
-  io = require('socket.io')(http);
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', function(socket){
+  console.log('we have a connection');
+  socket.on('new-message', function(msg){
+    console.log(msg);
+    io.emit('receive-message', msg);
+  });
+  socket.on("test",function(){
+    console.log('Mounted');
+  });
+});
+
+
+
+app.get('/', function (req, res) {
+  res.render('public/index.html', {});
+});
+
+http.listen('3000', function(){
+  console.log('we are connected');
+});
