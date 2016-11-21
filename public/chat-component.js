bcc770e06cb0bec720f5f76c3c04ae3769ad2679
maksimgm <maksimmamrikov@gmail.com>
2016-11-20 18:18:28 -0800

    add nodemon module to package.json

--- public/chat-component.js
+++ public/chat-component.js
@@ -1,4 +1,15 @@
 var ChatApp = React.createClass({
+  getInitalState: function(){
+    return{
+      messages: [],
+      socket: window.io('https://localhost:3000')
+    }
+  },
+
+  componentDidMount: function(){
+
+  },
+
   render: function(){
     return(
       <div>
